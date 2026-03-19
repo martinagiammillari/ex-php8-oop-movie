cc66aec8252752334d40344457ff4c7c3537e2bf
martinagiammillari <martigiammi@gmail.com>
2026-03-19 16:51:51 +0100

    feat: bonus 1 done

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ echo "Benvenuto nel database film! <br><br>";
 class Genre
 {
     public $name;
-    public $description;
+
 
     //COSTRUTTORE GENRE
-    public function __construct($name, $description)
+    public function __construct($name,)
     {
         $this->name = $name;
-        $this->description = $description;
+        
     }
 
 }
@@ -29,18 +29,28 @@ class Movie
     public $release;
     public $director;
     public $language;
-    public $genre;
+    public $genres;
 
     //COSTRUTTORE MOVIE
-    function __construct($name, $release, $director, Genre $genre, $language)
+    function __construct($name, $release, $director, array $genres, $language)
     {
         $this->name = $name;
         $this->release = $release;
         $this->director = $director;
-        $this->genre = $genre;
+        $this->genres = $genres;
         $this->language = $language;
 
 
+    }
+
+    // MEOTDO MOVIE PER OTTENERE TUTTI I NOMI GENERI 
+    public function getGenresString() {
+        $names = [];
+        foreach ($this->genres as $genre) {
+            // Estraiamo il nome da ogni oggetto Genre nell'array
+            $names[] = $genre->name;
+        }
+        return implode(", ", $names); // Unisce i nomi con una virgola
     }
 
 
@@ -59,27 +69,33 @@ class Movie
 
 
 //ISTANZIO 2 OGGETTI GENRE
-$action = new Genre("Azione", "Film con inseguimenti, combattimenti e molta adrenalina.");
-$drama = new Genre("Drammatico", "Film che trattano temi seri e profondi legati alla condizione umana.");
+$action = new Genre("Azione");
+$sciFi = new Genre("Fantascienza");
+$drama = new Genre("Drammatico");
 
 
 
 //ISTAZIO 2 OGGETTI MOVIE
-$film1 = new Movie("Oppenheimer", 2023, "Christopher Nolan", $drama, "Inglese");
-$film2 = new Movie("Pulp Fiction", 1994, "Quentin Tarantino", $action, "Inglese");
+$movie1 = new Movie("Oppenheimer", 2023, "Christopher Nolan", [$action, $sciFi], "Inglese");
+$movie2 = new Movie("Pulp Fiction", 1994, "Quentin Tarantino", [$action, $drama], "Inglese");
 
 //STAMPA DEI RISULTATI
-echo "--- DETTAGLI FILM ---<br>";
-echo "Titolo: " . $film1->name . "<br>";
-echo "Anno: " . $film1->release . "<br>";
-echo "Regista: " . $film1->director . "<br>";
-echo "Genere: " . $film1->genre->name . "<br>"; 
-echo "Descrizione Genere: " . $film1->genre->description . "<br>";
-echo "Recente? " . $film1->isRecent() . "<br><br>";
+echo "--- DETTAGLI FILM 1 ---<br>";
+echo "Titolo: " . $movie1->name . "<br>";
+echo "Anno: " . $movie1->release . "<br>";
+echo "Regista: " . $movie1->director . "<br>";
+echo "Lingua: " . $movie1->language . "<br>";
+echo "Generi: " . $movie1->getGenresString() . "<br>"; // Usiamo il metodo per l'array
+echo "Recente? " . $movie1->isRecent() . "<br>";
+
+echo "<br>-----------------------<br><br>";
 
 echo "--- DETTAGLI FILM 2 ---<br>";
-echo "Titolo: " . $film2->name . "<br>";
-echo "Genere: " . $film2->genre->name . "<br>";
-echo "Recente? " . $film2->isRecent() . "<br>";
+echo "Titolo: " . $movie2->name . "<br>";
+echo "Anno: " . $movie2->release . "<br>";
+echo "Regista: " . $movie2->director . "<br>";
+echo "Lingua: " . $movie2->language . "<br>";
+echo "Generi: " . $movie2->getGenresString() . "<br>"; // Usiamo il metodo per l'array
+echo "Recente? " . $movie2->isRecent() . "<br>";
 
 ?>
